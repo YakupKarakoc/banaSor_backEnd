f01fa0815cac6331cebd09f41e08a06043255dce
YakupKarakoc <krkcyakupp@gmail.com>
2025-03-12 18:47:17 +0300

    Vercel için gerekli backend düzenlemeleri yapıldı.

--- config/db.js
+++ config/db.js
@@ -4,11 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
